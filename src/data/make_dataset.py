694e6b64ed9c8522bd38c665c60119d2d7f526a7
Dr Dilek Celik <dilekcell40@gmail.com>
2025-09-07 22:00:24 +0100

    Update make_dataset.py
    Data paths

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 def load_data(filepath):
-    """Load raw dataset."""
-    return pd.read_csv(filepath)
+    # Read Training Data
+    df_productdetails = pd.read_csv(os.path.join(filepath, "ProductDetails.csv"))
+    df_cataloguediscontinuation = pd.read_csv(os.path.join(filepath, "CatalogueDiscontinuation.csv"))
+    return df_productdetails, df_cataloguediscontinuation
